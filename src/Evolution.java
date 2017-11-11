42c18575caeb7dd800bf9b2dbd9b041cad7a023e
danielchurch <daniellchurch@fvcc.edu>
2017-11-11 12:57:32 -0700

    Format

--- src/Evolution.java
+++ src/Evolution.java
@@ -1,8 +1,5 @@
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ThreadLocalRandom;
 import java.util.Random;
-import java.util.Optional;
 
 public class Evolution {
 
@@ -16,33 +13,33 @@ public class Evolution {
         GA, ES, DE
     }
 
-    public List selectParents() {
-        return selectParents();
+    public List<Chromosome> selectParents() {
+        return null;
     }
 
     public Chromosome crossover(List<Network> neuralNetwork){
         return chromosome;
     }
-	
+
 	public Chromosome mutation (Chromosome child, Chromosome evoStrat) {
-		for (int i = 0; i < child.adjacencyMatrix.length; ++i) {
-			for (int j = i + 1; j < child.adjacencyMatrix [i].length; ++j) {
-				int randomNum = ThreadLocalRandom.current().nextInt(0, mutationChance);
+		for (int i = 0; i < child.adjacencyMatrix.length; i++) {
+			for (int j = i + 1; j < child.adjacencyMatrix[i].length; j++) {
+				int randomNum = (int)(Math.random() * mutationChance);
 				if (randomNum == 0) {
 					if (evoStrat == null) {
-						double creep = randomInRange (minRange, maxRange);
-						child.adjacencyMatrix [i][j] = child.adjacencyMatrix [i][j] + creep;
+						double creep = randomInRange(minRange, maxRange);
+						child.adjacencyMatrix[i][j] = child.adjacencyMatrix[i][j] + creep;
 					}
 					else {
-						double creep = randomInRange (0, 1);
-						child.adjacencyMatrix [i][j] = child.adjacencyMatrix [i][j] + evoStrat.adjacencyMatrix [i][j] * creep;
+						double creep = randomInRange(0, 1);
+						child.adjacencyMatrix[i][j] = child.adjacencyMatrix[i][j] + evoStrat.adjacencyMatrix[i][j] * creep;
 					}
-				}					
+				}
 			}
 		}
 		return child;
 	}
-	
+
 	private static double randomInRange(double min, double max) {
 		double range = max - min;
 		double scaled = random.nextDouble() * range;
