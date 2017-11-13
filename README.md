74315d7d2bb8882b36dd584fb4642d50bc983681
Daniel Church <daniellchurch@fvcc.edu>
2017-11-13 12:48:28 -0700

    Remove gui (#28)
    * Add basic logging system
    
    * Remove javafx code and res
    
    * remove a bit more

--- README.md
+++ README.md
@@ -6,23 +6,19 @@ Running without any parameters will open the GUI.  If you would rather use the c
 
 For help with the command line parameters, use `java -jar evolution.jar -h`
 
-
 | Flag   | Description                                                  | Default   | Parameter |
 |--------|--------------------------------------------------------------|:---------:|:---------:|
-| -nogui | Runs the application without a GUI                           | true      | Void      |
+| -a     | Algorithm (bp, ga, ds, de)                                   | ga        | String    |
+| -b     | Beta Rate                                                    | 0.100     | Double    |
+| -f     | Data File                                                    | iris.data | String    |
 | -h     | Displays the help text                                       |           | Void      |
 | -hl    | The amount of hidden layers, and the amount of nodes in each | 40,40     | String    |
-| -s     | Save the weights to a given output file                      |           | String    |
-| -p     | Population size                                              | 10        | Integer   |
-| -o     | Offspring count                                              | 10        | Integer   |
 | -lr    | Learning Rate                                                | 0.010     | Double    |
-| -a     | Algorithm (bp, ga, ds, de)                                   | ga        | String    |
-| -f     | Data File                                                    | iris.data | String    |
 | -m     | Mutation Rate                                                | 0.050     | Double    |
-| -b     | Beta Rate                                                    | 0.100     | Double    |
-
+| -o     | Offspring count                                              | 10        | Integer   |
+| -p     | Population size                                              | 10        | Integer   |
 
 Example:
 ```java
-java -jar evolution.jar -nogui
+java -jar evolution.jar -f res/iris.data
 ```
--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,17 @@
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.stage.FileChooser;
-import javafx.stage.FileChooser.ExtensionFilter;
-import javafx.stage.Stage;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
-public class Main extends Application {
+public class Main {
 
     public static List<Integer> hiddenLayers;
     public static int populationSize;
     public static int offspringCount;
     public static String algorithm;
-    public static double mutationRate;
     public static String dataFilePath;
 
-    private static Stage primaryStage;
-    private static GUIController controller;
-    private static Network network;
-    private static boolean shouldStop = false;
     public static boolean shouldPause = false;
-    private static boolean useGUI = true;
-    private static String savePath;
-    private static Thread networkRun;
-    private static javax.swing.Timer timer;
-    private static double progress = 0;
-    private static int direction = 1;
 
     public static void start(String algorithm, List<Integer> hiddenLayers, int populationSize, int numOfChildren) {
         Logger.log("Starting");
@@ -61,121 +37,7 @@ public class Main extends Application {
 
         Logger.log("Starting to run evolution network");
 
-
         evolution.run();
-
-        // "Test" the progress bar
-        if (useGUI) {
-            timer = new javax.swing.Timer(1, ae -> {
-                if ((int) (Math.random() * 4) != 0) {
-                    progress += direction * 0.0001d;
-                    if(progress >= 1d) {
-                        progress = 1;
-                        direction = -1;
-                    } else if(progress <= 0) {
-                        progress = 0;
-                        direction = 1;
-                    }
-                    controller.progressBar.setProgress(progress);
-                }
-                if (shouldStop)
-                    timer.stop();
-            });
-
-            timer.start();
-        }
-    }
-
-    public static void load() {
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Choose a file to save the weights to.");
-        fileChooser.getExtensionFilters().add(new ExtensionFilter("Weights", "*.w8"));
-
-        List<Layer> layers = new ArrayList<Layer>();
-
-        File fileToOpen = fileChooser.showOpenDialog(primaryStage);
-        try {
-            Scanner scanner = new Scanner(fileToOpen);
-            while(scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                if(line.trim().startsWith("l")) { // Layer
-                    String info = line.split(":")[1].trim();
-                    // Get type
-                    Type type;
-                    if (info.equals("INPUT")) {
-                        type = Type.INPUT;
-                    } else if (info.equals("OUTPUT")) {
-                        type = Type.OUTPUT;
-                    } else if (info.equals("RBFINPUT")) {
-                        type = Type.RBFINPUT;
-                    } else if (info.equals("RBFHIDDEN")) {
-                        type = Type.RBFHIDDEN;
-                    } else if (info.equals("HIDDEN")) {
-                        type = Type.HIDDEN;
-                    } else {
-                        type = Type.HIDDEN;
-                    }
-
-                    List<Node> nodes = new ArrayList<Node>();
-                    while((line = scanner.nextLine()).trim().startsWith("n")) {
-                        List<Double> weights = new ArrayList<Double>();
-                        Stream.of(line.split("n:")[1].split(",")).forEach(s -> weights.add(Double.parseDouble(s.trim())));
-                        Node n = new Node(type, weights.size());
-                        n.weights = weights;
-                        nodes.add(n);
-                    }
-
-                    Layer layer = new Layer(nodes.size(), type);
-                    layer.nodes = nodes;
-                    layers.add(layer);
-                }
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void save(String filename) {
-        File fileToSave = null;
-
-        if (filename.isEmpty()) {
-            FileChooser fileChooser = new FileChooser();
-            fileChooser.setTitle("Choose a file to save the weights to.");
-            fileChooser.getExtensionFilters().add(new ExtensionFilter("Weights", "*.w8"));
-            fileToSave = fileChooser.showSaveDialog(primaryStage);
-        } else {
-            fileToSave = new File(filename);
-        }
-
-        if (fileToSave == null) return;
-
-        try {
-            final PrintWriter writer = new PrintWriter(fileToSave);
-
-            /* Save to file in the following format
-            l: TYPE
-                n: w1, w2, w3, w4, w5
-                n: w1, w2, w3, w4, w5
-            l: TYPE
-                n: w1, w2, w3, w4, w5
-                n: w1, w2, w3, w4, w5
-             */
-
-            network.layers.forEach(layer -> {
-                        writer.print("l: " + layer.layerType);
-                        layer.nodes.forEach(node -> {
-                                    writer.print("\tn: ");
-                                    node.weights.stream()
-                                            .map(weight -> weight + ", ")
-                                            .forEach(writer::print);
-                                    writer.println();
-                                });
-                        writer.println();
-                    });
-            writer.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
     }
 
     private static boolean printHelp() {
@@ -208,17 +70,15 @@ public class Main extends Application {
     }
 
     private static CommandLineParameter[] commands = {
-            new CommandLineParameter("-nogui", "Runs the application without a GUI",                           f -> useGUI = false,                    true, CommandLineParameter.Type.Void),     // No GUI
-            new CommandLineParameter("-h",     "Displays the help text",                                       f -> printHelp(),                       null, CommandLineParameter.Type.Void),     // Help
-            new CommandLineParameter("-hl",    "The amount of hidden layers, and the amount of nodes in each", s -> parseHiddenLayers((String) s),  "40,40", CommandLineParameter.Type.String),   // Hidden Layers
-            new CommandLineParameter("-s",     "Save the weights to a given output file",                      s -> savePath = (String) s,               "", CommandLineParameter.Type.String),   // Save
-            new CommandLineParameter("-p",     "Population size",                                              i -> populationSize = (int) i,            10, CommandLineParameter.Type.Integer),  // Population Size
-            new CommandLineParameter("-o",     "Offspring count",                                              i -> offspringCount = (int) i,            10, CommandLineParameter.Type.Integer),  // Offspring Count
-            new CommandLineParameter("-lr",    "Learning Rate",                                                d -> Evolution.learningRate = (double) d,.01, CommandLineParameter.Type.Double),   // Learning Rate
-            new CommandLineParameter("-a",     "Algorithm (bp, ga, ds, de)",                                   s -> algorithm = (String) s,            "ga", CommandLineParameter.Type.String),   // Algorithm
-            new CommandLineParameter("-f",     "Data File",                                                    s -> dataFilePath = (String) s,  "iris.data", CommandLineParameter.Type.String),   // Data File
-            new CommandLineParameter("-m",     "Mutation Rate",                                              d -> Evolution.mutationChance = (double) d,.05, CommandLineParameter.Type.Double),   // Mutation Rate
-            new CommandLineParameter("-b",     "Beta Rate",                                                    d -> Evolution.beta = (double) d,         .1, CommandLineParameter.Type.Double),   // Beta Rate
+            new CommandLineParameter("-a",     "Algorithm (bp, ga, ds, de)",                                   s -> algorithm = (String) s,            "ga",   CommandLineParameter.Type.String),   // Algorithm
+            new CommandLineParameter("-b",     "Beta Rate",                                                    d -> Evolution.beta = (double) d,         .1,   CommandLineParameter.Type.Double),   // Beta Rate
+            new CommandLineParameter("-f",     "Data File",                                                    s -> dataFilePath = (String) s,  "iris.data",   CommandLineParameter.Type.String),   // Data File
+            new CommandLineParameter("-h",     "Displays the help text",                                       f -> printHelp(),                       null,   CommandLineParameter.Type.Void),     // Help
+            new CommandLineParameter("-hl",    "The amount of hidden layers, and the amount of nodes in each", s -> parseHiddenLayers((String) s),  "40,40",   CommandLineParameter.Type.String),   // Hidden Layers
+            new CommandLineParameter("-lr",    "Learning Rate",                                                d -> Evolution.learningRate = (double) d,.01,   CommandLineParameter.Type.Double),   // Learning Rate
+            new CommandLineParameter("-m",     "Mutation Rate",                                                d -> Evolution.mutationChance = (double) d,.05, CommandLineParameter.Type.Double),   // Mutation Rate
+            new CommandLineParameter("-o",     "Offspring count",                                              i -> offspringCount = (int) i,            10,   CommandLineParameter.Type.Integer),  // Offspring Count
+            new CommandLineParameter("-p",     "Population size",                                              i -> populationSize = (int) i,            10,   CommandLineParameter.Type.Integer),  // Population Size
     };
 
     public static void main(String[] args) {
@@ -248,30 +108,6 @@ public class Main extends Application {
         }
 
         start(algorithm, hiddenLayers, populationSize, offspringCount);
-        if (!savePath.isEmpty()) save(savePath);
-        System.exit(0);
-
-    }
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("format.fxml"));
-        primaryStage.setScene(new Scene(loader.load()));
-        controller = loader.getController();
-        primaryStage.titleProperty().set("Rosenbrock Function Approximator");
-        primaryStage.setResizable(false);
-        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("rosenbrock.jpg")));
-        primaryStage.show();
-        Main.primaryStage = primaryStage;
-    }
-
-    @Override
-    public void stop() throws Exception {
-        shouldStop = true;
-        if (networkRun != null)
-            networkRun.interrupt();
-            networkRun.stop();
-        super.stop();
     }
 
     /**
